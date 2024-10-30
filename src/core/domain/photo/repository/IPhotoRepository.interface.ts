0d59a087932bea7a4c333498cf18b87a093526d1
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2024-10-31 01:11:49 +0500

    ditch repos

--- src/core/domain/photo/repository/IPhotoRepository.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Photo } from "../entities/photo.entity";
-import { CreatePhotoDto } from "../dto/create-photo.dto";
-
-export interface IPhotoRepository {
-   findOne(id: number): Promise<Photo | null>;
-   create(createPhotoDto: CreatePhotoDto): Promise<Photo>;
-}
-
