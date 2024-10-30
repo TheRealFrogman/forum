c25254d54716a2c4506689d98df142af30d90f56
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2024-10-31 00:52:13 +0500

    little change

--- src/infrastructure/database/MyDatabase.ts
+++ src/infrastructure/database/MyDatabase.ts
@@ -1,13 +1,16 @@
 import { IMyDatabase } from "@/core/database/my-database.interface";
 import { Pool } from "pg"
 export class MyDatabase implements IMyDatabase {
-   private pool = new Pool({
-      user: process.env['DB_USER'],
-      host: process.env['DB_HOST'],
-      database: process.env['DB_NAME'],
-      password: process.env['DB_PASSWORD'],
-      port: Number(process.env['DB_PORT']),
-   })
+   constructor(
+      private pool = new Pool({
+         user: process.env['DB_USER'],
+         host: process.env['DB_HOST'],
+         database: process.env['DB_NAME'],
+         password: process.env['DB_PASSWORD'],
+         port: Number(process.env['DB_PORT']),
+      })
+   ) { }
+
    query<T>(cls: new (...args: any[]) => T, query: string, params?: unknown[]): Promise<T | T[] | null> {
       return new Promise((resolve, reject) => {
          this.pool.query(query, params).then((result) => {
