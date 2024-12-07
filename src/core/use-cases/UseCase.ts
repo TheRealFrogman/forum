514834eb21fe315da1a1e7d74fc59787d3226a59
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2024-12-07 16:10:01 +0500

    add user to canDo

--- src/core/use-cases/UseCase.ts
+++ src/core/use-cases/UseCase.ts
@@ -1,8 +1,9 @@
+import { User } from "../domain/user/entities/user.entity";
 import { EndpointResult } from "../routing/routes";
 import { EventPublisher } from "./events/EventPublisher";
 
 export abstract class UseCase<EventType extends object = object> extends EventPublisher<EventType> {
-   protected canDo(..._args: any[]): boolean {
+   protected canDo(user: User,...args: any[]): boolean {
       throw new Error("Not implemented");
    }
    abstract execute(...args: any[]): Promise<EndpointResult>
