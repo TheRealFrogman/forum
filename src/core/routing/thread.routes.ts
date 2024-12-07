063c514685a5b046bc1ff58c899ab4ba2b5b33bf
frogman <74905689+TheRealFrogman@users.noreply.github.com>
2024-12-07 15:04:38 +0500

    return it for dev mode

--- src/core/routing/thread.routes.ts
+++ src/core/routing/thread.routes.ts
@@ -23,9 +23,10 @@ export const threadRoutes: Routes<'/threads' | "/threads/all"> = {
    ["/threads/all"]: {
       GET:
          async () => {
-            // throw new Error("Not implemented");
-            // сделать авторизованным этот запрос
-            return await getAllThreads_UseCase.execute()
+            if (process.env["ENV"] === "dev")
+               return await getAllThreads_UseCase.execute();
+
+            throw new Error("Allowed only in dev environment");
          }
    },
    ["/threads"]: {
